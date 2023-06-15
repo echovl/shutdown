5478aee5121f83b1a9b6dc5c355e70a64a8d19f9
Alonso Villegas <alonsovillegas816@gmail.com>
2023-06-15 11:39:22 -0500

    add shutdown status

--- main.go
+++ main.go
@@ -1,16 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"os/exec"
 )
+
+var mustShutdownNow = false
+
+type Response struct {
+	MustShutdownNow bool `json:"must_shutdown_now"`
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Shutting down")
-		exec.Command("shutdown", "now").Run()
+		if r.Method == http.MethodPost {
+			mustShutdownNow = true
+		} else if r.Method == http.MethodDelete {
+			mustShutdownNow = false
+		}
+
+		resp := Response{mustShutdownNow}
+
+		fmt.Println(resp)
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(resp)
 	})
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
